be0e2dded4042a3972fa80502f940b45c3b1fcbc
Noah Edelstein <Noedelstein@gmail.com>
2020-10-19 15:44:51 -0400

    first commit

---
+++ sketch.js
@@ -0,0 +1,30 @@
+
+
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    document.body.style.margin="0px";
+  }
+  
+  
+  function draw() {
+    background(255, 252, 0);
+  
+    let size = width * 0.025;
+  
+    for (let o1 = size; o1 <= width - size; o1 += size * 2) {
+      for (let o2 = size; o2 <= height - size; o2 += size * 2) {
+  
+        push();
+        rectMode(CENTER);
+        translate(o1, o2);
+        let d = dist(mouseX, mouseY, o1, o2);
+        let dConstrained = constrain(d, 0, 100);
+        let rotation = frameCount * 0.02 * (dConstrained * 0.005);
+        rotate(rotation);
+        rect(0, 0, size, size * 1.5,3);
+        pop();
+      }
+    }
+  
+  }
